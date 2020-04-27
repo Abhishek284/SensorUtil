fe565355f2d0878cab91fa687df01d84be479793
Abhishek <abhishekmaharajpet@gmail.com>
2020-04-27 11:59:14 +0530

    Test

---
+++ app/src/main/java/com/lib/sensorutilexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lib.sensorutilexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ sensorutil/src/main/java/com/lib/sensorutil/LogUtil.kt
@@ -0,0 +1,9 @@
+package com.lib.sensorutil
+
+import android.util.Log
+
+object LogUtil {
+    fun logData(string: String) {
+        Log.d("MYLIBRARY", string)
+    }
+}
