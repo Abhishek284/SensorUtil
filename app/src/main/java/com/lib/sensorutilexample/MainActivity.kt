e8630332b5a4870d5f0446ed0588600bd53c70c6
Abhishek <abhishekmaharajpet@gmail.com>
2020-04-27 14:12:04 +0530

    Added accelerometer

--- app/src/main/java/com/lib/sensorutilexample/MainActivity.kt
+++ app/src/main/java/com/lib/sensorutilexample/MainActivity.kt
@@ -2,11 +2,35 @@ package com.lib.sensorutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , AccelerometerManager.AccelerometerListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AccelerometerManager.isSupported(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        AccelerometerManager.startListening(this)
+    }
+
+    override fun onAccelerationChanged(x: Float, y: Float, z: Float) {
+        Toast.makeText(this, " Accelerometer $x $y", Toast.LENGTH_SHORT).show();
+
+    }
+
+    override fun onShake(force: Float) {
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (AccelerometerManager.isListening) {
+            AccelerometerManager.stopListening();
+
+            Toast.makeText(this, "onDestroy Accelerometer Stopped", Toast.LENGTH_SHORT).show();
+        }
     }
 }
